345860ea1b72f25e67f090439aab76be10b37433
Arkadiusz Ciesielski <arkadiusz.ciesielski@poczta.onet.pl>
2020-09-22 22:28:07 +0200

    Display start new game button

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
